14596aab3de2b999038e7d9e76679921cfd25dfd
Valeria Romero <valellope@gmail.com>
2023-08-03 20:06:10 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Lista from './componentes/lista/Lista'
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Dojo!</h1>
+      <Lista></Lista>
+    </div>
+  );
+}
+
+export default App;
